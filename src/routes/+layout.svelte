3ba013da571bb900c5a6401b9daf91b11b8884cc
Paul Rodriguez <mildmoose@pm.me>
2024-01-23 21:15:15 -0700

    fixed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
     import { inject } from '@vercel/analytics';
     import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit"
     inject({ mode: dev ? 'development' : 'production' });
+    injectSpeedInsights()
 </script>
   
 <slot />
